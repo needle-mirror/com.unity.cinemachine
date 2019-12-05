3057dbc65d38286aa0cd27947fa0a5adea997b35
Unity Technologies <@unity.com>
2019-12-06 00:00:00 +0100

    com.unity.cinemachine@2.4.0-preview.10 ## [2.4.0-preview.10] - 2019-12-06 ### HDRP 7 support and bugfixes - Storyboard: added global mute function - New vcams are by default created matching the cene view camera - Added ApplyBeforeBody option to POV component, to support working with FramingTransposer - Added RectenterTarget to POV component - Added OnTransitionFromCamera callback to extensions - Added Damping to SameAsFollowTarget and HardLockToTarget components - URP 7.1.3: added CinemachinePixelPerfect extension - Added Speed Mode to AxisState, to support direct axis control without max speed - New example scene: OverTheShoulderAim illustrating how to do over-the-shoulder TPS cam, with Normal and Aim modes - Impulse Manager: added option to ignore timescale - Framing Transposer: added OnTransition handling for camera rotation if InheritPosition - Upgrade to support HDRP and Universal RP 7.0.0 API - Upgrade to support HDRP and Universal RP 7.1.0 API - Removed Resources diretories - Sample scenes now available via package manager - Added optional "Display Name" field to Cinemachine Shot in Timeline - Added "Adopt Current Camera Settings" item to vcam inspector context menu - Composer and FramingTransposer: allow the dead zone to extend to 2, and the Screen x,Y can range from -1.5 to 1.5 - HDRP: lens presets include physical settings if physical camera - Regression Fix: Framing Transposer: ignore LookAt target.  Use Follow exclusively - Bugfix: PostProcessing extension was not handling standby update correctly when on Manager Vcams - Bugfixes: (fogbugz 1193311, 1193307, 1192423, 1192414): disallow presets for vcams - Bugfix: In some heading modes, FreeLook was improperly modifying the axes when activated - Bugfix: Orbital transposer was improperly filtering the heading in TargetForward heading mode - Bugfix: added EmbeddedAssetHelper null check - Bugfix: composer screen guides drawn in correct place for physical camera - Bugfix: FreeLook was not respecting wait time for X axis recentering - Bugfix: FreeLook X axis was not always perfectly synched between rigs - Bugfix (fogbugz 1176866): Collider: clean up static RigidBody on exit - Bugfix (fogbugz 1174180): framing transposer wrong ortho size calculation - Bugfix (fogbugz 1158509): Split brain.UpdateMethod into VcamUpdateMethod and BrainUpdateMethod, to make blending work correctly - Bugfix (fogbugz 1162074): Framing transposer and group transposer only reached half maximum ortho size - Bugfix (fogbugz 1165599): Transposer: fix gimbal lock issue in LockToTargetWithWorldUp - Bugfix: VolumeSettings: handle layermask in HDAdditionalCameraData - Bugfix: use vcam's up when drawing gizmos (orbital transposer and free look)

--- Editor/Menus/CinemachineMenu.cs
+++ Editor/Menus/CinemachineMenu.cs
@@ -200,14 +200,17 @@ namespace Cinemachine.Editor
         [MenuItem("Cinemachine/Import Post Processing V2 Adapter Asset Package")]
         private static void ImportPostProcessingV2Package()
         {
-            string pkgFile = ScriptableObjectUtility.CinemachineInstallPath
-                + "/Extras~/CinemachinePostProcessingV2.unitypackage";
-            if (!File.Exists(pkgFile))
-                Debug.LogError("Missing file " + pkgFile);
-            else
-                AssetDatabase.ImportPackage(pkgFile, true);
+            var message = "In Cinemachine 2.4.0 and up, the PostProcessing adapter is built-in, and "
+                + "can be auto-enabled by Unity 2019 and up.\n\n"
+                + "Unity 2018.4 is unable to auto-detect the presence of PostProcessing, so you must "
+                + "manually add a define to your player settings to enable the code.\n\n"
+                + "To enable support for PostProcessing v2, please do the following:\n\n"
+                + "1. Delete the CinemachinePostProcessing folder from your assets, if it's present\n\n"
+                + "2. Open the Player Settings tab in Project Settings\n\n"
+                + "3. Add this define: CINEMACHINE_POST_PROCESSING_V2";
+
+            EditorUtility.DisplayDialog("Cinemachine Adapter Code for PostProcessing V2", message, "OK");
         }
-#endif
 
         [MenuItem("Cinemachine/Import CinemachineExamples Asset Package")]
         private static void ImportExamplePackage()
@@ -219,6 +222,7 @@ namespace Cinemachine.Editor
             else
                 AssetDatabase.ImportPackage(pkgFile, true);
         }
+#endif
 
         /// <summary>
         /// Create a default Virtual Camera, with standard components
@@ -248,13 +252,9 @@ namespace Cinemachine.Editor
             GameObject go = InspectorUtility.CreateGameObject(
                     GenerateUniqueObjectName(typeof(CinemachineVirtualCamera), name),
                     typeof(CinemachineVirtualCamera));
-            if (SceneView.lastActiveSceneView != null)
-            {
-                go.transform.position = SceneView.lastActiveSceneView.camera.transform.position;
-                go.transform.rotation = SceneView.lastActiveSceneView.camera.transform.rotation;
-            }
-            Undo.RegisterCreatedObjectUndo(go, "create " + name);
             CinemachineVirtualCamera vcam = go.GetComponent<CinemachineVirtualCamera>();
+            SetVcamFromSceneView(vcam);
+            Undo.RegisterCreatedObjectUndo(go, "create " + name);
             GameObject componentOwner = vcam.GetComponentOwner().gameObject;
             foreach (Type t in components)
                 Undo.AddComponent(componentOwner, t);
@@ -264,6 +264,16 @@ namespace Cinemachine.Editor
             if (selectIt)
                 Selection.activeObject = go;
             return vcam;
+        }
+
+        public static void SetVcamFromSceneView(CinemachineVirtualCamera vcam)
+        {
+            if (SceneView.lastActiveSceneView != null)
+            {
+                vcam.transform.position = SceneView.lastActiveSceneView.camera.transform.position;
+                vcam.transform.rotation = SceneView.lastActiveSceneView.camera.transform.rotation;
+                vcam.m_Lens = LensSettings.FromCamera(SceneView.lastActiveSceneView.camera);
+            }
         }
 
         /// <summary>
